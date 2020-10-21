619a72d80de12561cdf494d031159763936f7996
Dez <dezmaniquemartin6@gmail.com>
2020-10-21 16:52:15 -0400

    created Driver class, printing fruits and vegetables

--- PartnerFunWithMonicaAndDez/src/com/beans/Food.java
+++ PartnerFunWithMonicaAndDez/src/com/beans/Food.java
@@ -6,8 +6,8 @@ public class Food {
 	public static void main(String[] args) {
 		Vegetable lettuce = new Vegetable("green", "circle", false);
 		Fruit tomato = new Fruit("red", "circle", true);
-		System.out.println(lettuce + " Monica");
-		System.out.println(tomato + " Monica");
+		System.out.println(lettuce + " Dez");
+		System.out.println(tomato + " Dez");
 	}
 	
 	
