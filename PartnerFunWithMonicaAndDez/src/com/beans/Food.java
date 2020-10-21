044e7acd6d0165554752b931b9556ba6f8e6c8fe
Dez <dezmaniquemartin6@gmail.com>
2020-10-21 17:07:55 -0400

    created beans and driver

--- PartnerFunWithMonicaAndDez/src/com/beans/Food.java
+++ PartnerFunWithMonicaAndDez/src/com/beans/Food.java
@@ -6,8 +6,8 @@ public class Food {
 	public static void main(String[] args) {
 		Vegetable lettuce = new Vegetable("green", "circle", false);
 		Fruit tomato = new Fruit("red", "circle", true);
-		System.out.println(lettuce + " Dez");
-		System.out.println(tomato + " Dez");
+		System.out.println(lettuce);
+		System.out.println(tomato);
 	}
 	
 	
